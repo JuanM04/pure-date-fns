3e24030422d261298c5cf38c240a1e436c2166af
JuanM04 <me@juanm04.com>
2021-11-28 17:08:12 -0300

    🩹 Fix `clamp` import

--- src/clamp.ts
+++ src/clamp.ts
@@ -1,7 +1,8 @@
 import original from "date-fns/clamp"
 
 import type {Interval} from "./_types"
-import {fromJSDate, intervalToDateInterval} from "./_utils"
+import {intervalToDateInterval} from "./_utils"
+import fromJSDate from "./fromJSDate"
 import toJSDate from "./toJSDate"
 
 export default function clamp(date: string, interval: Interval): string {
