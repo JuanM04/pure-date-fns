40a9485576ae85c18d67ad795e13c427d8e7098a
JuanM04 <me@juanm04.com>
2021-11-28 21:08:04 -0300

    🚚 Moved lib files into subfolders

--- src/_lib/intervalToDateInterval/index.ts
+++ src/_lib/intervalToDateInterval/index.ts
@@ -1,7 +1,7 @@
 import type {Interval as OriginalInterval} from "date-fns"
 
-import toJSDate from "../toJSDate"
-import type {Interval} from "./types"
+import toJSDate from "../../toJSDate"
+import type {Interval} from "../types"
 
 export const intervalToDateInterval = (interval: Interval): OriginalInterval => ({
   start: toJSDate(interval.start),
